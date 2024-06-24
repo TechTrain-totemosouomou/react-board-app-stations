b27296ce1fcabd8bb4f5094e6f89fc5c4b3b8900
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-25 02:19:06 +0900

    スレッド新規作成画面：遷移のみ

---
+++ src/NewThreadScreen.jsx
@@ -0,0 +1,14 @@
+// NewThreadScreen.jsx
+
+import { Link } from 'react-router-dom';
+
+const NewThreadScreen = () => {
+  return (
+    <div>
+      <Link to="/">新着スレッド</Link>
+      <h2>新規スレッド作成</h2>
+    </div>
+  );
+};
+
+export default NewThreadScreen;
